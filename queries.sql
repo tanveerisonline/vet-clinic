637f7293fa6f9f62a3ca441eedaddd89ed173168
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-02-25 22:22:20 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,31 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
-
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE neutered = true;
+SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT name FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
-
-
-BEGIN;
-update animals set species = 'unspecified';
-select * from animals;
-rollback;
-select * from animals;
-
-UPDATE animals set species = 'digimon' where name like '%mon';
-select * from animals;
-update animals set species = 'pokemon' where species = '';
-select * from animals;
-
-BEGIN;
-delete from animals;
-ROLLBACK;
-select * from animals;
-=======
 SELECT name FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 /* Query to update and read the animals table data */
@@ -60,10 +40,31 @@ SELECT COUNT(*) FROM animals; -- verify that the rollback happened
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SAVEPOINT here;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO here;
+UPDATE animals
+SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
+/* Query to select animals table data with specific condition and also count them */
+SELECT COUNT() FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT ROUND(AVG(weight_kg)::numeric, 2) FROM animals;
+SELECT name FROM animals
+WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals GROUP BY species;
+SELECT species, ROUND(AVG(escape_attempts)::numeric, 0) FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
 
+/* Query to verify changes made before and after the commit */
+BEGIN;
+SELECT species from animals; -- verify original values
+UPDATE animals SET species = 'test';
+SELECT species from animals; -- verify changes made
+ROLLBACK;
+SELECT species from animals; -- verify that change was rolled back
+COMMIT;
+SELECT species from animals; -- verify that change persists after commit
