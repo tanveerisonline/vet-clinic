0ca159e40aa4c43583a210ac115f73a49b38cb2f
tanveerisonline <tanveerisonline@gmail.com>
2023-02-25 11:31:31 +0530

    readme removed

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE,
-    escape_attempts INTEGER,
-    neutered BOOLEAN,
-    weight_kg DECIMAL(4, 2)
-);
