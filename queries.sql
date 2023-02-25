3b3f6aa8fc72cb672fa574cf0a43bba4e7f03ea2
tanveerisonline <tanveerisonline@gmail.com>
2023-02-25 14:18:51 +0530

    Created multiple tables

--- queries.sql
+++ queries.sql
@@ -1,54 +1,87 @@
-/*Queries that provide answers to the questions from all projects.*/
-
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
+SELECT * FROM animals WHERE neutered = true;
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT name FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT name FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
-
-
-/* Query to update and read the animals table data */
 
 BEGIN;
-UPDATE animals
-SET species = 'unspecified';
+update animals set species = 'unspecified';
+select * from animals;
+rollback;
+select * from animals;
+
+UPDATE animals set species = 'digimon' where name like '%mon';
+select * from animals;
+update animals set species = 'pokemon' where species = '';
+select * from animals;
+
+BEGIN;
+delete from animals;
 ROLLBACK;
-
-BEGIN;
-UPDATE animals
-SET species = 'digimon'
-WHERE name Like '%mon';
-
-UPDATE animals
-SET species = 'pokemon'
-WHERE species IS NULL;
-COMMIT;
-
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
+select * from animals;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT here;
-UPDATE animals
-SET weight_kg = weight_kg * -1;
-ROLLBACK TO here;
-UPDATE animals
-SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
-
-/* Query to select animals table data with specific condition and also count them */
 
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
-SELECT ROUND(AVG(weight_kg)::numeric, 2) FROM animals;
-SELECT name FROM animals 
-  WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
-SELECT species, MIN(weight_kg), MAX(weight_kg) 
-  FROM animals GROUP BY species;
-SELECT species, ROUND(AVG(escape_attempts)::numeric, 0) FROM animals 
-  WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31' GROUP BY species;
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) as avg_escapes
+FROM animals
+GROUP BY neutered
+ORDER BY avg_escapes DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31'
+GROUP BY species;
+
+SELECT name FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT animals.name FROM animals
+JOIN species ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+
+SELECT owners.full_name, animals.name
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id;
+
+SELECT species.name, COUNT(animals.id) as count
+FROM species
+LEFT JOIN animals ON species.id = animals.species_id
+GROUP BY species.id;
+
+SELECT animals.name
+FROM animals
+JOIN species ON animals.species_id = species.id
+JOIN owners ON animals.owner_id = owners.id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
+
+SELECT owners.full_name, COUNT(animals.id) as count
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.id
+ORDER BY count DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,29 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-   id bigserial PRIMARY KEY,
-    name  VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     date_of_birth DATE,
-    escape_attempts INT,
+    escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg Decimal
+    weight_kg DECIMAL(4, 2)
 );
 
-ALTER TABLE animals
-ADD species VARCHAR(255);
+alter table animals add species VARCHAR(250);
+
+create table owners (
+    id serial primary key, 
+    full_name varchar(255), 
+    age integer
+);
+
+create table species (
+    id serial primary key, 
+    name varchar(255)
+);
+
+alter table animals drop column species;
+alter table animals add species_id integer;
+alter table animals add foreign key(species_id) references species(id);
+
+alter table animals add owner_id integer;
+
+alter table animals add foreign key(owner_id) references owners(id);
